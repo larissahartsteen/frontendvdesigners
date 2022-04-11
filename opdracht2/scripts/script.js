f998e2bedcb1cf24e7392dce4ff90caf71495e48
larissahartsteen <94127726+larissahartsteen@users.noreply.github.com>
2022-04-11 14:34:18 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,42 +1,65 @@
-// JavaScript Document
-console.log("howdy");
+// console.log("hallo");
 
-var filterGrid = document.querySelector("#filter-grid");
-var filterList = document.querySelector("#filter-list");
-var filterLocation = document.querySelector("#filter-location");
-var filterTagged = document.querySelector("#filter-tagged");
+///////////////////////////
+///////////////////////////
+// variabelen /////////////
+///////////////////////////
+///////////////////////////
+var aantalFollowers = 920;
+var followButton = document.getElementById("followButton");
 
-
-
-
-/* laat de radio buttons luisteren naar wijzigingen */
-// addEventListener
-// roep dan steeds dezelfde functie aan
-
-filterGrid.addEventListener("change", filterenMaar);
-filterList.addEventListener("change", filterenMaar);
-filterLocation.addEventListener("change", filterenMaar);
-filterTagged.addEventListener("change", filterenMaar);
+var gridFilter = document.querySelector("#filter-grid");
+var listFilter = document.querySelector("#filter-list");
+var locationFilter = document.querySelector("#filter-location");
+var taggedFilter = document.querySelector("#filter-tagged");
 
 
-/* maak die functie aan */
-// zoek de ul op en stop die in een variabele
-// sla de value van de gekozen radio button op in een variabele --> event.target.value
-// verwijder de huidige class van de ul --> deLijst.className = "";
-// voeg de de nieuwe value als class toe aan de ul --> gebruik daarvoor de variabele van 2 regels omhoog
+
+
+
+///////////////////////////
+///////////////////////////
+// follow & unfollow button
+///////////////////////////
+///////////////////////////
+followButton.onclick = function() { //als follow op button wordt geklikt --> deze function uitgevoerd
+    if (followButton.innerText == "+ Follow") { // met innerText wordt de tekstinhoud van een element ingesteld of geretourneerd --> https://www.w3schools.com/jsref/prop_node_innertext.asp
+        aantalFollowers++;
+        followButton.innerText = "Unfollow";
+        // wanneer de tekst gelijk staat aan (==) '+ Follow' wordt er 1 bij opgeteld (++) en wordt de tekst veranderd naar 'Unfollow'
+    } else if (followButton.innerText == "Unfollow") {
+        aantalFollowers--;
+        followButton.innerText = "+ Follow";
+    }   // anders.. waneer de tekst gelijk staat aan (==) 'Unfollow' wordt er 1 bij afgetrokke (--) en wordt de tekst veranderd naar '+ Follow'
+        followers.innerHTML = aantalFollowers; // met innerHTML wordt de HTML-inhoud (innerlijke HTML) van een element ingesteld of geretourneerd --> https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+}       // het aantal followers (Id in HTML) wordt (=) het nieuwe aantal volgers
+
+
+
+
+
+
+///////////////////////////
+///////////////////////////
+// filter met radiobuttons // --> https://codepen.io/shooft/pen/NWwdaro?editors=0010
+///////////////////////////
+///////////////////////////
+
+gridFilter.addEventListener("change", filterenMaar); // de radio buttons luisteren naar wijzigingen en roepen steeds dezelfde functie aan
+listFilter.addEventListener("change", filterenMaar);
+locationFilter.addEventListener("change", filterenMaar);
+taggedFilter.addEventListener("change", filterenMaar);
+
 
 function filterenMaar(event) {
-  let hetNieuweFilter = event.target.value;
+  let hetNieuweFilter = event.target.value; // met event.target.value sla je de value van de gekozen radio button op in een variabele
   
-  let deLijst = document.querySelector("section:nth-of-type(3) ul");
+  let deLijst = document.querySelector("section:nth-of-type(2) ul"); // zoekt de ul op en stop deze in een variabele
   
-  // oude class weghalen - ik weet alleen niet welke van de 4 - daarom allemaal
-  deLijst.classList.remove("grid");
+  deLijst.classList.remove("grid"); // verwijderd de huidige class van de ul
   deLijst.classList.remove("list");
   deLijst.classList.remove("location");
   deLijst.classList.remove("tagged");
   
-  // nieuwe class toevoegen
-  deLijst.classList.add(hetNieuweFilter);
-  
+  deLijst.classList.add(hetNieuweFilter); // voegt de nieuwe waarde als class toe aan de ul
 }
