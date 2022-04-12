c0486d57b9c892faa28842e8266d9308f28c973a
larissahartsteen <94127726+larissahartsteen@users.noreply.github.com>
2022-04-12 02:50:57 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,19 +1,22 @@
-// console.log("hallo");
-
 ///////////////////////////
 ///////////////////////////
 // variabelen /////////////
 ///////////////////////////
 ///////////////////////////
 var aantalFollowers = 920;
-var followButton = document.getElementById("followButton");
+var followButton = document.querySelector("#followButton");
 
-var gridFilter = document.querySelector("#filter-grid");
-var listFilter = document.querySelector("#filter-list");
+var optionList = document.querySelector("#view-list");
+var optionGrid = document.querySelector("#view-grid");
+var optionSlideshow = document.querySelector("#view-slideshow");
 var locationFilter = document.querySelector("#filter-location");
 var taggedFilter = document.querySelector("#filter-tagged");
 
+var previousButton = document.querySelector(".prev");
+var nextButton = document.querySelector(".next");
+var slides = document.getElementsByClassName('inList')
 
+var currentSlide = 0;
 
 
 
@@ -41,25 +44,103 @@ followButton.onclick = function() { //als follow op button wordt geklikt --> dez
 
 ///////////////////////////
 ///////////////////////////
-// filter met radiobuttons // --> https://codepen.io/shooft/pen/NWwdaro?editors=0010
+// filter met radiobuttons  --> https://codepen.io/shooft/pen/NWwdaro?editors=0010 
+/////////////////////////// --> https://codepen.io/shooft/pen/YzENrWE?editors=1000
 ///////////////////////////
-///////////////////////////
-
-gridFilter.addEventListener("change", filterenMaar); // de radio buttons luisteren naar wijzigingen en roepen steeds dezelfde functie aan
-listFilter.addEventListener("change", filterenMaar);
+optionList.addEventListener("change", showInList); // de radio buttons luisteren naar wijzigingen en voeren de function showInList, showInGrid of filterenMaar uit
+optionGrid.addEventListener("change", showInGrid);
+optionSlideshow.addEventListener("change", showInSlideshow);
 locationFilter.addEventListener("change", filterenMaar);
 taggedFilter.addEventListener("change", filterenMaar);
 
 
 function filterenMaar(event) {
-  let hetNieuweFilter = event.target.value; // met event.target.value sla je de value van de gekozen radio button op in een variabele
-  
-  let deLijst = document.querySelector("section:nth-of-type(2) ul"); // zoekt de ul op en stop deze in een variabele
-  
-  deLijst.classList.remove("grid"); // verwijderd de huidige class van de ul
-  deLijst.classList.remove("list");
-  deLijst.classList.remove("location");
-  deLijst.classList.remove("tagged");
-  
-  deLijst.classList.add(hetNieuweFilter); // voegt de nieuwe waarde als class toe aan de ul
+    let hetNieuweFilter = event.target.value; // met event.target.value sla je de value van de gekozen radio button op in een variabele
+    
+    let deLijst = document.querySelector("section:nth-of-type(2) ul"); // zoekt de ul op en stop deze in een variabele
+
+    deLijst.classList.remove("grid"); // verwijderd de huidige class van de ul
+    deLijst.classList.remove("list");
+    deLijst.classList.remove("slideshow");
+    deLijst.classList.remove("location");
+    deLijst.classList.remove("tagged");
+    
+    deLijst.classList.add(hetNieuweFilter); // voegt de nieuwe waarde als class toe aan de ul
 }
+
+
+function showInList() {
+    let deLijst = document.querySelector("section:nth-of-type(2) ul"); // lijst in html wordt opgezocht
+    deLijst.classList.remove("grid-view", "slideshow-view"); // class grid-view & slideshow)view worden verwijderd
+    deLijst.classList.add("list-view"); // class list-view wordt toegevoegd
+}
+
+function showInGrid() {
+    let deLijst = document.querySelector("section:nth-of-type(2) ul"); // lijst in html wordt opgezocht
+    deLijst.classList.remove("list-view", "slideshow-view"); // class list-view & slideshow-view worden verwijderd
+    deLijst.classList.add("grid-view"); // class grid-view wordt toegevoegd
+}
+
+function showInSlideshow() {
+    let deLijst = document.querySelector("section:nth-of-type(2) ul"); // lijst in html wordt opgezocht
+    deLijst.classList.remove("grid-view", "list-view"); // class grid-view & list-view worden verwijderd
+    deLijst.classList.add("slideshow-view"); // class slideshow-view wordt toegevoegd
+}
+
+
+
+
+
+///////////////////////////
+///////////////////////////
+// slideshow //////////////
+///////////////////////////
+///////////////////////////
+// Code that moves to the next slide
+nextButton.addEventListener("click", function() {
+        slides[currentSlide].classList.remove("active");
+        currentSlide ++;
+    if (currentSlide === slides.length)
+        currentSlide = 0;
+        slides[currentSlide].classList.add("active");
+});
+
+
+// Code that moves to the previous slide
+previousButton.addEventListener("click", function () {
+        slides[currentSlide].classList.remove("active");
+        currentSlide --;
+    if (currentSlide < 0)
+        currentSlide = (slides.length - 1);
+        slides[currentSlide].classList.add("active");
+});
+
+
+
+
+
+
+
+
+
+  
+//   function toggleImage(){ // --> https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+//     var lijstMetLI = document.querySelectorAll('section:nth-of-type(2) ul li'); 
+//     lijstMetLI.forEach(function(value, index, lijstObject) { // voor elk element in de lijst pak je uit de lijst, roept functie aan, verwacht drie
+//         value.classList.toggle('hidden')
+//     })
+//   }
+ 
+  
+  
+//   window.onload = function () {
+//     if (sessionStorage.getItem("modal") === "none") {
+//       document.getElementById("modal").style.display = "none";
+//     }
+//     document.getElementById("close").onclick = function () {
+//       document.getElementById("modal").style.display = "none";
+//       sessionStorage.setItem("modal", "none");
+//     };
+//   };
+  
+  
