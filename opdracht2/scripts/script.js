c5ba3ada64718f679f145fcf0609ee72f0469945
larissahartsteen <94127726+larissahartsteen@users.noreply.github.com>
2022-04-13 12:04:21 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -3,8 +3,8 @@
 // variabelen /////////////
 ///////////////////////////
 ///////////////////////////
-var aantalFollowers = 920;
-var followButton = document.querySelector("#followButton");
+var aantalFollowers = 920; // geeft nu 920 mee zodat als je de site opent je deze meteen al ziet
+var followButton = document.querySelector("#followButton"); // zoekt de ID in html en geeft deze een variabel
 
 var optionList = document.querySelector("#view-list");
 var optionGrid = document.querySelector("#view-grid");
@@ -15,8 +15,14 @@ var taggedFilter = document.querySelector("#filter-tagged");
 var previousButton = document.querySelector(".prev");
 var nextButton = document.querySelector(".next");
 var slides = document.getElementsByClassName('inList')
+var currentSlide = 0; 
 
-var currentSlide = 0;
+var deBanaan = document.querySelector('section:nth-of-type(3).swipeOverBanaan div');
+var touchstartX = 0;
+var touchendX = 0;
+var bewegingsZone = deBanaan; // var bewegingsZone wordt deBanaan
+
+
 
 
 
@@ -25,15 +31,15 @@ var currentSlide = 0;
 // follow & unfollow button
 ///////////////////////////
 ///////////////////////////
-followButton.onclick = function() { //als follow op button wordt geklikt --> deze function uitgevoerd
+followButton.onclick = function() { // als op follow button wordt geklikt --> deze function uitgevoerd
     if (followButton.innerText == "+ Follow") { // met innerText wordt de tekstinhoud van een element ingesteld of geretourneerd --> https://www.w3schools.com/jsref/prop_node_innertext.asp
         aantalFollowers++;
         followButton.innerText = "Unfollow";
-        // wanneer de tekst gelijk staat aan (==) '+ Follow' wordt er 1 bij opgeteld (++) en wordt de tekst veranderd naar 'Unfollow'
+        // wanneer de tekst (nummer) gelijk staat aan (==) '+ Follow' wordt er 1 bij opgeteld (++) en wordt de tekst veranderd naar 'Unfollow'
     } else if (followButton.innerText == "Unfollow") {
         aantalFollowers--;
         followButton.innerText = "+ Follow";
-    }   // anders.. waneer de tekst gelijk staat aan (==) 'Unfollow' wordt er 1 bij afgetrokke (--) en wordt de tekst veranderd naar '+ Follow'
+    }   // anders.. waneer de tekst gelijk staat aan (==) 'Unfollow' wordt er 1 bij afgetrokken (--) en wordt de tekst veranderd naar '+ Follow'
         followers.innerHTML = aantalFollowers; // met innerHTML wordt de HTML-inhoud (innerlijke HTML) van een element ingesteld of geretourneerd --> https://www.w3schools.com/jsref/prop_html_innerhtml.asp
 }       // het aantal followers (Id in HTML) wordt (=) het nieuwe aantal volgers
 
@@ -47,8 +53,8 @@ followButton.onclick = function() { //als follow op button wordt geklikt --> dez
 // filter met radiobuttons  --> https://codepen.io/shooft/pen/NWwdaro?editors=0010 
 /////////////////////////// --> https://codepen.io/shooft/pen/YzENrWE?editors=1000
 ///////////////////////////
-optionList.addEventListener("change", showInList); // de radio buttons luisteren naar wijzigingen en voeren de function showInList, showInGrid of filterenMaar uit
-optionGrid.addEventListener("change", showInGrid);
+optionList.addEventListener("change", showInList); // de radio buttons luisteren naar wijzigingen en voeren de function showInList, showInGrid, showInSlideshow of filterenMaar uit
+optionGrid.addEventListener("change", showInGrid); // event wordt geactiveerd wanneer een wijziging in de waarde van het element wordt doorgevoerd door de gebruiker
 optionSlideshow.addEventListener("change", showInSlideshow);
 locationFilter.addEventListener("change", filterenMaar);
 taggedFilter.addEventListener("change", filterenMaar);
@@ -95,46 +101,45 @@ function showInSlideshow() {
 ///////////////////////////
 ///////////////////////////
 nextButton.addEventListener("click", function() { // als je op de 'nextButton' klikt wordt de volgende function uitgevoerd
-        slides[currentSlide].classList.remove("active"); 
-        currentSlide ++;
-    if (currentSlide === slides.length)
-        currentSlide = 0;
-        slides[currentSlide].classList.add("active");
+        slides[currentSlide].classList.remove("active"); // var 'slides' neemt waarde van var 'currentSlide' en verwijderd de class 'active'
+        currentSlide ++; // er wordt 1 bij opgeteld (++), de volgende slide wordt weergegeven
+    if (currentSlide === slides.length) // === gelijk aan, moet zelfde type zijn zoals strings --> slides.length wijst naar de laatste slide in de lijst
+        currentSlide = 0; // als de 'cuurentSlide' gelijk staan aan de lengte van de slides (laatste) gaat deze terug naar de eerste slide (0)
+        slides[currentSlide].classList.add("active"); // var 'slides' neemt waarde van var 'currentSlide' en voegt hier de class 'active' aan toe
 });
 
 
-previousButton.addEventListener("click", function () { // als je op de 'nextButton' klikt wordt de volgende function uitgevoerd
-        slides[currentSlide].classList.remove("active");
+previousButton.addEventListener("click", function () { 
+        slides[currentSlide].classList.remove("active"); 
         currentSlide --;
     if (currentSlide < 0)
-        currentSlide = (slides.length - 1);
+        currentSlide = (slides.length - 1); 
         slides[currentSlide].classList.add("active");
 });
+
 
 
 function doorSlidesMetToetsen(event) {
-    switch(event.key) {/* bepalen welke toets is ingedrukt */
+    switch(event.key) { // bepaald welke toets is ingedrukt --> switch voert een codeblok uit, afhankelijk van verschillende acties
 
-    case "ArrowRight":/* als dat de ArrowRight is dan dit doen */  
-        slides[currentSlide].classList.remove("active");
-        currentSlide --;
-    if (currentSlide < 0)
-        currentSlide = (slides.length - 1);
-        slides[currentSlide].classList.add("active");
-    break; /* break - zodat de andere toetsen niet onnodig gecheckt worden */
+    case "ArrowLeft": // als dat de ArrowLeft is dan dit doen
+        slides[currentSlide].classList.remove("active"); // var 'slides' neemt waarde van var 'currentSlide' en verwijderd de class 'active'
+        currentSlide --; // er wordt 1 bij afgetrokken (--), de vorige slide wordt weergegeven
+    if (currentSlide < 0) // als de 'currentSlide' een lagere waarde heeft dan 0..
+        currentSlide = (slides.length - 1); // gaat het terug naar de laatste foto in de rij
+        slides[currentSlide].classList.add("active"); // var 'slides' neemt waarde van var 'currentSlide' en voegt hier de class 'active' aan toe
+    break; // break: het breekt uit de switch --> stopt de uitvoering van meer code in de switch
 
-    case "ArrowLeft": /* als dat de ArrowLeft is dan dit doen */
+    case "ArrowRight": // als dat de ArrowRight is dan dit doen
         slides[currentSlide].classList.remove("active");
         currentSlide ++;
     if (currentSlide === slides.length)
         currentSlide = 0;
         slides[currentSlide].classList.add("active");
-    break; /* break - zodat de andere toetsen niet onnodig gecheckt worden */
-    // case "Escape":
-    // break; 
+    break; // break: het breekt uit de switch --> stopt de uitvoering van meer code in de switch
     }
 }
-document.addEventListener('keydown', doorSlidesMetToetsen); /* het document luistert naar toetsaanslagen */ /* bij een toetsaanslag wordt de functie openMenuMetToetsen uitgevoerd */
+document.addEventListener('keydown', doorSlidesMetToetsen); // het document luistert naar toetsaanslagen --> bij een toetsaanslag wordt de functie doorSlidesMetToetsen uitgevoerd
   
   
   
@@ -146,17 +151,12 @@ document.addEventListener('keydown', doorSlidesMetToetsen); /* het document luis
 // swipen /////////////////
 ///////////////////////////
 ///////////////////////////
-var deBanaan = document.querySelector('section:nth-of-type(3).swipeOverBanaan div');
-var touchstartX = 0;
-var touchendX = 0;
-var bewegingsZone = deBanaan;
-
-bewegingsZone.addEventListener('touchstart', function(event) {
-    touchstartX = event.changedTouches[0].screenX;
+bewegingsZone.addEventListener('touchstart', function(event) { // touchstart vindt plaats wanneer de gebruiker een element 'touched'
+    touchstartX = event.changedTouches[0].screenX; // touchstartX is een lijst met de aanraakpunten die actief zijn geworden met de huidige gebeurtenis 
 }, false);
 
 bewegingsZone.addEventListener('touchend', function(event) {
-    touchendX = event.changedTouches[0].screenX;
+    touchendX = event.changedTouches[0].screenX; // touchendX is een lijst van de aanraakpunten die van het oppervlak zijn verwijderd (de reeks aanraakpunten die overeenkomen met vingers die het oppervlak niet meer raken)
     handleGesure();
 }, false); 
 
@@ -168,6 +168,14 @@ function handleGesure() {
         deBanaan.classList.remove("erIsOverMijGeswipet");
     }
 }
+
+
+
+
+
+
+
+
   
   
 
